525c4b60f6a3a39b8647ba00b3eafd11c07a166b
sharyar-dev <onshariiit@gmail.com>
2025-04-05 20:30:22 +0500

    feat: add GitHub and upload icons, enhance generated ticket display

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,8 @@ import { useForm } from 'vee-validate'
 import { toTypedSchema } from '@vee-validate/zod'
 import { z } from 'zod'
 import IconInfo from '@/components/IconInfo.vue'
+import IconGithub from '@/components/IconGithub.vue'
+import IconUpload from '@/components/IconUpload.vue'
 
 const schema = toTypedSchema(
   z.object({
@@ -141,7 +143,42 @@ const onSubmit = handleSubmit((values) => {
           </p>
         </div>
 
-        <div></div>
+        <div class="relative w-[343px] h-[160px] md:w-[600px] md:h-[280px]">
+          <img
+            src="/images/pattern-ticket.svg"
+            class="absolute top-0 right-0 z-30"
+            alt="line top-right"
+          />
+          <div class="z-40 p-6 flex flex-col justify-between h-full relative">
+            <div class="flex gap-5">
+              <img src="/images/logo-mark.svg" alt="logo" />
+              <div>
+                <div class="text-preset-2-mobile md:text-preset-2">Coding Conf</div>
+                <div class="text-preset-6 text-neutral-300">Jan 31, 2025 / Austin, TX</div>
+              </div>
+            </div>
+            <div class="flex items-center gap-4">
+              <img
+                v-if="avatarPreview"
+                :src="avatarPreview"
+                class="w-[80px] h-[80px] object-cover rounded-xl"
+                :alt="fullName"
+              />
+              <div class="flex flex-col gap-2">
+                <div class="text-preset-3-mobile md:text-preset-3">{{ fullName }}</div>
+                <div class="flex gap-2">
+                  <IconGithub />
+                  <span>{{ githubUsername }}</span>
+                </div>
+              </div>
+              <div
+                class="absolute top-[50%] right-4 md:right-8 translate-y-[-50%] text-neutral-500 text-preset-3-mobile md:text-preset-3 [writing-mode:vertical-lr]"
+              >
+                #01609
+              </div>
+            </div>
+          </div>
+        </div>
       </template>
       <template v-else>
         <div class="flex flex-col items-center text-center gap-5 mb-12">
@@ -169,30 +206,9 @@ const onSubmit = handleSubmit((values) => {
                   v-if="avatarPreview"
                   :src="avatarPreview"
                   class="w-[30px] h-[30px] object-cover"
-                  alt=""
+                  alt="Your avatar"
                 />
-                <svg
-                  v-else
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="30"
-                  height="30"
-                  fill="none"
-                  viewBox="0 0 30 30"
-                  class="text-orange-500"
-                >
-                  <path
-                    fill="currentColor"
-                    fill-rule="evenodd"
-                    d="M21.894 11.252a.264.264 0 0 1-.229-.225c-.368-2.634-2.51-5.924-6.663-5.924-4.465 0-6.3 3.636-6.657 5.928a.264.264 0 0 1-.228.22c-2.95.362-4.945 2.622-4.945 5.729a5.802 5.802 0 0 0 3.423 5.277 6.274 6.274 0 0 0 2.305.468h2.528a.45.45 0 0 0 .45-.45c0-.267-.233-.472-.5-.484a3.077 3.077 0 0 1-2.049-.9 3.123 3.123 0 0 1 0-4.418l3.461-3.462c.147-.146.307-.277.479-.392.076-.05.158-.085.236-.129.1-.054.196-.114.301-.158.1-.04.206-.065.308-.096.092-.027.181-.062.276-.081.191-.039.384-.056.578-.059.011 0 .022-.004.034-.004.01 0 .018.003.027.004.196.002.391.02.584.059.094.019.18.053.271.08.105.031.211.055.313.098.1.042.193.098.288.15.084.046.17.083.25.137.154.103.295.221.428.349.016.014.034.024.049.039l3.463 3.463a3.124 3.124 0 0 1 0 4.42c-.558.56-1.284.86-2.05.897-.266.013-.497.219-.497.486 0 .249.202.451.451.451h2.512c.435 0 1.314-.06 2.344-.473a5.794 5.794 0 0 0 3.394-5.272c0-3.104-1.991-5.363-4.935-5.728Z"
-                    clip-rule="evenodd"
-                  />
-                  <path
-                    fill="currentColor"
-                    fill-rule="evenodd"
-                    d="M18.464 19.62a.936.936 0 0 0 .663-1.6l-3.464-3.464a.938.938 0 0 0-.664-.275l-.014.002a.932.932 0 0 0-.65.274l-3.462 3.462a.936.936 0 1 0 1.326 1.325l1.864-1.862v6.479a.937.937 0 1 0 1.875 0v-6.48l1.864 1.863a.93.93 0 0 0 .662.275Z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
+                <IconUpload v-else />
               </span>
               <p v-if="isOverDropZone" class="text-preset-6 text-neutral-300">Drop it here</p>
               <div v-else-if="avatarPreview" class="flex items-center gap-2">
